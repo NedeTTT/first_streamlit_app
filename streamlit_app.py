4c5555aa91e53cd7150be48741dce3eeb5d5d66d
Nedelea Teodor Valeriu <114878346+NedeTTT@users.noreply.github.com>
2022-10-05 21:17:39 +0300

    Update streamlit_app.py
    xz

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
    streamlit.error("Please select a fruit to get information.")
    else:
       
-streamlit.write('The user entered ', fruit_choice)
+#streamlit.write('The user entered ', fruit_choice)
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
